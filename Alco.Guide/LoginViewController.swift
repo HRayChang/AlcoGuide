4c2d4c4babb705a21b718c663901e6c0390bb13f
RayChang <raychang861205@gmail.com>
2023-11-15 19:24:17 +0800

    extension uicolor

---
+++ Alco.Guide/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Alco.Guide
+//
+//  Created by Ray Chang on 2023/11/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.valentino
+    }
+
+
+}
+
+
---
+++ Alco.Guide/UIColor+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+Extension.swift
+//  Alco.Guide
+//
+//  Created by Ray Chang on 2023/11/15.
+//
+
+import UIKit
+
+extension UIColor {
+    static var lilac: UIColor {
+        return UIColor(hex: 0xD3B7D8)
+    }
+    
+    static var steelPink: UIColor {
+        return UIColor(hex: 0xA13E97)
+    }
+    
+    static var eminence: UIColor {
+        return UIColor(hex: 0x632A7E)
+    }
+    
+    static var valentino: UIColor {
+        return UIColor(hex: 0x280E3B)
+    }
+    
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
