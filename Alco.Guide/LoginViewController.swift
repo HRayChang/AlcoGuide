8611caa1c7b372e3eb8d9604f708ff600d83ad9c
RayChang <raychang861205@gmail.com>
2023-11-15 19:37:24 +0800

    setup login page

--- Alco.Guide/LoginViewController.swift
+++ Alco.Guide/LoginViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    let loginButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = UIColor.valentino
+        
+        setupLoginButton()
     }
-
-
+    
+    func setupLoginButton() {
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.layer.borderWidth = 2.0
+        loginButton.layer.borderColor = UIColor.steelPink.cgColor
+        loginButton.layer.backgroundColor = UIColor.eminence.cgColor
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(loginButton)
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func loginButtonTapped() {
+        let mapHomeViewController = MapHomeViewController()
+        navigationController?.pushViewController(mapHomeViewController, animated: true)
+    }
 }
-
-
