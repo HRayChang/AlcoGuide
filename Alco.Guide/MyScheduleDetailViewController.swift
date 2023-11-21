bd1f87e052a42ab12552dcd94504ed0b736a7d6f
RayChang <raychang861205@gmail.com>
2023-11-21 22:08:05 +0800

    hide the cell when drag

--- Alco.Guide/MyScheduleDetailViewController.swift
+++ Alco.Guide/MyScheduleDetailViewController.swift
@@ -65,34 +65,33 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
         ])
     }
     
-//    @objc func editButtonTapped() {
-//        tableView.setEditing(!tableView.isEditing, animated: true)
-//                             
-//        if (!tableView.isEditing) {
-//    
-//            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "編輯", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
-//            
-//        } else {
-//
-//            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "完成", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
-//            
-//            self.view.endEditing(true)
-//
-//        }
-//    }
-    
-    // MARK: - Drag
+    // MARK: - Drag & Drop
     func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         
         var dragItem: UIDragItem?
         
         if indexPath.row == 0 {
             
+            // Drag the whole section
             let draggedItem = CurrentSchedule.currentLocations![indexPath.section]
             dragItem = UIDragItem(itemProvider: NSItemProvider(object: draggedItem as NSItemProviderWriting))
             
+            // Hidden cells when drag
+            tableView.performBatchUpdates({
+                let section = indexPath.section
+                let numberOfRows = tableView.numberOfRows(inSection: section)
+                
+                for row in 1..<numberOfRows {
+                    let indexPathToHide = IndexPath(row: row, section: section)
+                    if let cell = tableView.cellForRow(at: indexPathToHide) {
+                        cell.isHidden = true
+                    }
+                }
+            }, completion: nil)
+            
         } else {
             
+            // Drag the single cell
             dragItem = UIDragItem(itemProvider: NSItemProvider())
         }
         
@@ -100,7 +99,7 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
     }
     
     func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
-
+        
         guard var sourceIndexPath = coordinator.items.first?.sourceIndexPath,
               var destinationIndexPath = coordinator.destinationIndexPath else {
             return
@@ -108,10 +107,12 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
         
         if sourceIndexPath.row == 0 {
             
+            
             if sourceIndexPath.section == destinationIndexPath.section {
                 return
             }
             
+            // Change the whole section
             let draggedItem = CurrentSchedule.currentLocations![sourceIndexPath.section]
             CurrentSchedule.currentLocations!.remove(at: sourceIndexPath.section)
             CurrentSchedule.currentLocations!.insert(draggedItem, at: destinationIndexPath.section)
@@ -121,47 +122,39 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
             }, completion: nil)
             
         } else {
+        
             
-            if destinationIndexPath.row == 0 || destinationIndexPath.row == CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![destinationIndexPath.section]]!.count - 1 {
+            if destinationIndexPath.row == 0 || destinationIndexPath.row == CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![destinationIndexPath.section]]!.count + 2 {
                 return
             }
             
+            // Change the cell index
             let draggedItem = CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![sourceIndexPath.section]]![sourceIndexPath.row - 1]
             CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![sourceIndexPath.section]]!.remove(at: sourceIndexPath.row - 1)
             CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![destinationIndexPath.section]]!.insert(draggedItem, at: destinationIndexPath.row - 1)
             
-            //            if let indexPath = coordinator.destinationIndexPath {
-            //                print(indexPath)
-            //                destinationIndexPath = indexPath
-            //            } else {
-            //                // 如果目标 indexPath 为 nil，说明拖曳到了一个空白区域
-            //                // 这里可以根据实际需求处理
-            //
-            //                return
-            //            }
-            // 在这里处理数据源的更新
-            // 例如，将源 indexPath 的数据移动到目标 indexPath
-            // 注意，这里的更新操作可能涉及你的数据源数组，具体实现取决于你的数据结构
-            // 这里只是一个示例，实际情况需要根据你的数据源结构进行调整
-            //            let draggedItem = CurrentSchedule.currentLocations![sourceIndexPath.row]
-            //            CurrentSchedule.currentLocations!.remove(at: sourceIndexPath.row)
-            //            CurrentSchedule.currentLocations!.insert(draggedItem, at: destinationIndexPath.row)
-            
-            // 在动画中更新 tableView
+            // Update the tableView with animation
             tableView.performBatchUpdates({
                 tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
             }, completion: nil)
         }
+        
         tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
         
+        // Show hidden cells after drag
+        UIView.animate(withDuration: 0.3) {
+            tableView.visibleCells.forEach { $0.isHidden = false }
+        }
+        
         // Allow dropping anywhere in the table view
         let isSectionDraggable = destinationIndexPath != nil
         return UITableViewDropProposal(operation: isSectionDraggable ? .move : .forbidden, intent: .insertAtDestinationIndexPath)
+    
     }
-    // MARK: Drag -
+    // MARK: Drag & Drop -
     
     // MARK: - TableView
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -230,23 +223,14 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
         }
     }
     
-//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//    
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//        }
-//    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    
-//    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//    
-//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-//
-//        tableView.reloadData()
-//    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+        }
+    }
     // MARK: TableView -
 }
