3fa26d6b77db3054d4ca4aae69cbdf840f287fb2
RayChang <raychang861205@gmail.com>
2023-11-21 21:13:53 +0800

    complete drag and drop

--- Alco.Guide/MyScheduleDetailViewController.swift
+++ Alco.Guide/MyScheduleDetailViewController.swift
@@ -6,9 +6,8 @@
 //
 
 import UIKit
-import FirebaseFirestore
 
-class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITableViewDragDelegate, UITableViewDropDelegate {
     
     let tableView = UITableView()
     
@@ -20,20 +19,25 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.dragDelegate = self
+         tableView.dropDelegate = self
+         tableView.dragInteractionEnabled = true
+        
         setupMyScheduleDetailViewUI()
         setupConstraints()
         
         tableView.register(MyScheduleDetailActivitiesTableViewCell.self, forCellReuseIdentifier: "ActivitiesCell")
         tableView.register(MyScheduleDetailLocationNameTableViewCell.self, forCellReuseIdentifier: "LocationNameCell")
         tableView.register(MyScheduleDetailButtonTableViewCell.self, forCellReuseIdentifier: "ButtonCell")
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
         
-        tableView.setEditing(true, animated: false)
-        editButtonTapped()
+//        tableView.setEditing(true, animated: false)
+//        editButtonTapped()
         
     }
     
@@ -61,22 +65,105 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
         ])
     }
     
-    @objc func editButtonTapped() {
-        tableView.setEditing(!tableView.isEditing, animated: true)
-                             
-        if (!tableView.isEditing) {
+//    @objc func editButtonTapped() {
+//        tableView.setEditing(!tableView.isEditing, animated: true)
+//                             
+//        if (!tableView.isEditing) {
+//    
+//            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "編輯", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
+//            
+//        } else {
+//
+//            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "完成", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
+//            
+//            self.view.endEditing(true)
+//
+//        }
+//    }
     
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "編輯", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
+    // MARK: - Drag
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        
+        var dragItem: UIDragItem?
+        
+        if indexPath.row == 0 {
+            
+            let draggedItem = CurrentSchedule.currentLocations![indexPath.section]
+            dragItem = UIDragItem(itemProvider: NSItemProvider(object: draggedItem as NSItemProviderWriting))
             
         } else {
-
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "完成", style: .plain, target: self, action: #selector(MyScheduleViewController.editButtonTapped))
             
-            self.view.endEditing(true)
-
+            dragItem = UIDragItem(itemProvider: NSItemProvider())
         }
+        
+        return [dragItem!]
     }
     
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+
+        guard var sourceIndexPath = coordinator.items.first?.sourceIndexPath,
+              var destinationIndexPath = coordinator.destinationIndexPath else {
+            return
+        }
+        
+        if sourceIndexPath.row == 0 {
+            
+            if sourceIndexPath.section == destinationIndexPath.section {
+                return
+            }
+            
+            let draggedItem = CurrentSchedule.currentLocations![sourceIndexPath.section]
+            CurrentSchedule.currentLocations!.remove(at: sourceIndexPath.section)
+            CurrentSchedule.currentLocations!.insert(draggedItem, at: destinationIndexPath.section)
+            
+            tableView.performBatchUpdates({
+                tableView.moveSection(sourceIndexPath.section, toSection: destinationIndexPath.section)
+            }, completion: nil)
+            
+        } else {
+            
+            if destinationIndexPath.row == 0 || destinationIndexPath.row == CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![destinationIndexPath.section]]!.count - 1 {
+                return
+            }
+            
+            let draggedItem = CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![sourceIndexPath.section]]![sourceIndexPath.row - 1]
+            CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![sourceIndexPath.section]]!.remove(at: sourceIndexPath.row - 1)
+            CurrentSchedule.currentActivities![CurrentSchedule.currentLocations![destinationIndexPath.section]]!.insert(draggedItem, at: destinationIndexPath.row - 1)
+            
+            //            if let indexPath = coordinator.destinationIndexPath {
+            //                print(indexPath)
+            //                destinationIndexPath = indexPath
+            //            } else {
+            //                // 如果目标 indexPath 为 nil，说明拖曳到了一个空白区域
+            //                // 这里可以根据实际需求处理
+            //
+            //                return
+            //            }
+            // 在这里处理数据源的更新
+            // 例如，将源 indexPath 的数据移动到目标 indexPath
+            // 注意，这里的更新操作可能涉及你的数据源数组，具体实现取决于你的数据结构
+            // 这里只是一个示例，实际情况需要根据你的数据源结构进行调整
+            //            let draggedItem = CurrentSchedule.currentLocations![sourceIndexPath.row]
+            //            CurrentSchedule.currentLocations!.remove(at: sourceIndexPath.row)
+            //            CurrentSchedule.currentLocations!.insert(draggedItem, at: destinationIndexPath.row)
+            
+            // 在动画中更新 tableView
+            tableView.performBatchUpdates({
+                tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+            }, completion: nil)
+        }
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        
+        // Allow dropping anywhere in the table view
+        let isSectionDraggable = destinationIndexPath != nil
+        return UITableViewDropProposal(operation: isSectionDraggable ? .move : .forbidden, intent: .insertAtDestinationIndexPath)
+    }
+    // MARK: Drag -
+    
+    // MARK: - TableView
     func numberOfSections(in tableView: UITableView) -> Int {
         
         if let currentLocations = CurrentSchedule.currentLocations {
@@ -135,32 +222,31 @@ class MyScheduleDetailViewController: UIViewController, UITableViewDelegate, UIT
             
             let activities = currentActivities[location]
             
-            print(currentActivities[location])
-            
             let activity = activities?[indexPath.row - 1]
                 cell.activityLabel.text = activity
-            print("!!!!!!!!\(activity)")
+            
             return cell
             
         }
     }
     
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//        }
+//    }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-        }
-    }
     
-    
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-
-        tableView.reloadData()
-    }
+//    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    
+//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+//
+//        tableView.reloadData()
+//    }
+    // MARK: TableView -
 }
